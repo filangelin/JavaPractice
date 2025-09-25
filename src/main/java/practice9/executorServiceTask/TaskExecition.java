31c9e7bae55828601e3d90a58f96e25bb1fc94c8
Angelina Matveeva <fil.angeliin@gmail.com>
2025-09-25 23:11:30 +0300

    refactor TaskExecution

--- src/main/java/practice9/executorServiceTask/TaskExecition.java
+++ src/main/java/practice9/executorServiceTask/TaskExecition.java
@@ -2,6 +2,7 @@ package practice9.executorServiceTask;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TaskExecition {
     public static void main(String[] args) throws InterruptedException {
@@ -11,7 +12,9 @@ public class TaskExecition {
         for (int i = 0; i < 10; i++) {
             executorService.submit(new Task(i));
         }
-
         executorService.shutdown();
+        while (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+            System.out.println("Задачи еще выполняются");
+        }
     }
 }
